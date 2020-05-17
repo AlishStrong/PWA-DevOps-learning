5f1148bbf2c7067fc3d79864a93578af9e094559
AlishStrong <alishet1990@gmail.com>
2020-05-17 15:19:16 +0300

    Reacts to on push events! Checks if tests failed or succeded. Save test results to test.log file if tests failed. Notifies if tests passed or failed.

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,11 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonCardTitle } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,9 +19,16 @@ describe('HomePage', () => {
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be MASTER title', () => {
+    expect(
+      debugElement.query(By.directive(IonCardTitle)).nativeElement.textContent
+      ).toContain('MASTER TO FAIL')
   });
 });
