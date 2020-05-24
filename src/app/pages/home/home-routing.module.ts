c100497c7398fa94546191b5922fffce642e3eef
AlishStrong <alishet1990@gmail.com>
2020-05-25 01:12:45 +0300

    Generated Grid and Map pages for tabs of the home page

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -6,39 +6,20 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-    children:
-      [
-        {
-          path: 'grid',
-          children:
-            [
-              {
-                path: '',
-                loadChildren: () => import('./grid/grid.module').then( m => m.GridPageModule)
-              }
-            ]
-        },
-        {
-          path: 'map',
-          children:
-            [
-              {
-                path: '',
-                loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
-              }
-            ]
-        },
-        {
-          path: '',
-          redirectTo: '/home/grid',
-          pathMatch: 'full'
-        }
-      ]
+  },
+  {
+    path: 'grid',
+    loadChildren: () => import('./grid/grid.module').then(m => m.GridPageModule)
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./map/map.module').then(m => m.MapPageModule)
   }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
