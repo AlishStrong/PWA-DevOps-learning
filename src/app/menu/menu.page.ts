52163fa93dd36600806b49327f3f580e5131021e
AlishStrong <alishet1990@gmail.com>
2020-05-24 12:02:13 +0300

    Split pane, Active Page style, separate Menu

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menu',
@@ -6,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
+  selectedPath = '/home';
+  pages = [
+    {
+      title: 'Home',
+      url: '/home'
+    },
+    {
+      title: 'Profile',
+      url: '/profile'
+    }
+  ]
 
-  constructor() { }
+  constructor(private router: Router) {
+   this.router.events.pipe(
+     filter((event: RouterEvent) => (event.url && event.url !== undefined && event.url !== '/'))
+   ).subscribe((event: RouterEvent) => {
+     this.selectedPath = event.url;
+   })
+  }
 
   ngOnInit() {
   }
