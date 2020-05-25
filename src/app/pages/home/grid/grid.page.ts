9e28bdd52a451953aee0c2bc28345e91ee5aed1d
Alisher <alisher.aliev@tieto.com>
2020-05-25 22:12:16 +0300

    Array of places

--- src/app/pages/home/grid/grid.page.ts
+++ src/app/pages/home/grid/grid.page.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid.page.scss'],
 })
 export class GridPage implements OnInit {
+  places: Place[] = [
+    { title: `Alisher's` },
+    { title: `Peppi's` },
+    { title: `Paulina and co.` },
+    { title: `Matisserie` },
+    { title: `Lebanon delights` },
+    { title: `Viihdemaa` },
+    { title: `Movie nation` },
+    { title: `Sea adventures` },
+    { title: `Sailor's bar` },
+    { title: `Seven friends` },
+    { title: `Vegicious` },
+    { title: `Body+` },
+    { title: `Laser polygon` },
+    { title: `Music bar` },
+    { title: `Karaoke, chill and bar` },
+    { title: 'Dupligo' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export interface Place {
+  title: string
 }
