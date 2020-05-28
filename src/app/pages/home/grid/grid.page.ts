d97bb934cfa35e52f2489634fd6c906e28d2a477
Alisher <alisher.aliev@tieto.com>
2020-05-28 22:24:37 +0300

    Specified types

--- src/app/pages/home/grid/grid.page.ts
+++ src/app/pages/home/grid/grid.page.ts
@@ -11,23 +11,84 @@ export class GridPage implements OnInit {
   places: Place[] = [
     {
       title: `Alisher's`,
-      img: 'alishers'
+      img: 'alishers',
+      type: ['eatery']
     },
-    { title: `Peppi's`, img: 'peppis' },
-    { title: `Paulina and co.`, img: 'paulina_and_co' },
-    { title: `Matisserie`, img: 'matisserie' },
-    { title: `Lebanon delights`, img: 'lebanon_delights' },
-    { title: `Viihdemaa`, img: 'viihdemaa' },
-    { title: `Movie nation`, img: 'movie_nation' },
-    { title: `Sea adventures`, img: 'sea_adventures' },
-    { title: `Sailor's bar`, img: 'sailors_bar' },
-    { title: `Seven friends`, img: 'seven_friends' },
-    { title: `Vegicious`, img: 'vegicious' },
-    { title: `Body+`, img: 'body_plus' },
-    { title: `Laser polygon`, img: 'laser_polygon' },
-    { title: `Music bar`, img: 'music_bar' },
-    { title: `Karaoke, chill and bar`, img: 'karaoke_chill_and_bar' },
-    { title: 'Dupligo', img: 'dupligo' }
+    {
+      title: `Peppi's`,
+      img: 'peppis',
+      type: ['eatery']
+    },
+    {
+      title: `Paulina and co.`,
+      img: 'paulina_and_co',
+      type: ['eatery']
+    },
+    {
+      title: `Matisserie`,
+      img: 'matisserie',
+      type: ['eatery']
+    },
+    {
+      title: `Lebanon delights`,
+      img: 'lebanon_delights',
+      type: ['eatery']
+    },
+    {
+      title: `Viihdemaa`,
+      img: 'viihdemaa',
+      type: ['active', 'movies', 'games', 'sports']
+    },
+    {
+      title: `Movie nation`,
+      img: 'movie_nation',
+      type: ['movies']
+    },
+    {
+      title: `Sea adventures`,
+      img: 'sea_adventures',
+      type: ['sports', 'games', 'water']
+    },
+    {
+      title: `Sailor's bar`,
+      img: 'sailors_bar',
+      type: ['eatery', 'bar', 'karaoke']
+    },
+    {
+      title: `Seven friends`,
+      img: 'seven_friends',
+      type: ['bar', 'eatery', 'night club']
+    },
+    {
+      title: `Vegicious`,
+      img: 'vegicious',
+      type: ['eatery']
+    },
+    {
+      title: `Body+`,
+      img: 'body_plus',
+      type: ['sports']
+    },
+    {
+      title: `Laser polygon`,
+      img: 'laser_polygon',
+      type: ['sports', 'games']
+    },
+    {
+      title: `Music bar`,
+      img: 'music_bar',
+      type: ['bar', 'eatery', 'karaoke', 'music']
+    },
+    {
+      title: `Karaoke, chill and bar`,
+      img: 'karaoke_chill_and_bar',
+      type: ['bar', 'karaoke', 'night club']
+    },
+    {
+      title: 'Dupligo',
+      img: 'dupligo',
+      type: ['eatery', 'bar', 'music']
+    }
   ];
 
   modalElement: HTMLIonModalElement;
@@ -56,5 +117,6 @@ export class GridPage implements OnInit {
 
 export interface Place {
   title: string,
-  img: string
+  img: string,
+  type?: string[]
 }
