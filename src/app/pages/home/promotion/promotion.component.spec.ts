10b460b6c14268b1c631601530a6dccb457a8315
Alisher <alisher.aliev@tieto.com>
2020-05-30 17:51:41 +0300

    Fixed tests

--- src/app/pages/home/promotion/promotion.component.spec.ts
+++ src/app/pages/home/promotion/promotion.component.spec.ts
@@ -1,7 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { Place } from '../grid/grid.page';
 import { PromotionComponent } from './promotion.component';
+
+
+const alishers: Place = {
+  title: `Alisher's`,
+  img: 'alishers',
+  type: ['eatery']
+}
 
 describe('PromotionComponent', () => {
   let component: PromotionComponent;
@@ -9,12 +16,13 @@ describe('PromotionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PromotionComponent ],
+      declarations: [PromotionComponent],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PromotionComponent);
     component = fixture.componentInstance;
+    component.selectedPlace = alishers;
     fixture.detectChanges();
   }));
 
