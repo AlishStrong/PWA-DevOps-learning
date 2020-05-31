0f0642a1eea8455c97cc1638e634f95ef6751493
Alisher <alisher.aliev@tieto.com>
2020-05-31 20:51:57 +0300

    Tokenized and Typisized the Leaflet library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { RouteReuseStrategy } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import * as leaflet from 'leaflet';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LEAFLET_TOKEN } from './leaflet/leaflet.service';
 import { LeafletmapComponent } from './leafletmap/leafletmap.component';
 
-
-
+// const leaflet: Leaflet =
 @NgModule({
   declarations: [AppComponent, LeafletmapComponent],
   entryComponents: [],
@@ -18,6 +19,10 @@ import { LeafletmapComponent } from './leafletmap/leafletmap.component';
     IonicModule.forRoot(),
     AppRoutingModule],
   providers: [
+    {
+      provide: LEAFLET_TOKEN,
+      useValue: leaflet
+    },
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/leafletmap/leafletmap.component.ts
+++ src/app/leafletmap/leafletmap.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { Leaflet, LEAFLET_TOKEN } from '../leaflet/leaflet.service';
 @Component({
   selector: 'app-leafletmap',
   template: `
@@ -15,24 +15,28 @@ style="height: 50vh; width: 50vw; border: 5px solid red;"></div>
 })
 export class LeafletmapComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(
+    @Inject(LEAFLET_TOKEN) private L: Leaflet
+  ) { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
-      console.log('20 msec passes');
-      var mymap = L.map('mapid').setView([60.1918347, 24.8373201], 13);
-
-      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
-
-      console.log(tiles);
+      console.log('I 100 msec passed');
+      const mymap = this.L.map('mapid');
+      console.log('create Map');
+      mymap.setView([60.1918347, 24.8373201], 13);
+      const tiles = this.L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          maxZoom: 19
+        }
+      );
       setTimeout(() => {
-        console.log('2 sec passes');
+        console.log('II 100 sec passed');
         tiles.addTo(mymap);
       }, 100);
     }, 100)
