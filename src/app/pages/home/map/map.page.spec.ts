ff7606c26996e4bd91f489eb149a8591e0d50211
Alisher <alisher.aliev@tieto.com>
2020-05-31 22:02:03 +0300

    fixed tests

--- src/app/pages/home/map/map.page.spec.ts
+++ src/app/pages/home/map/map.page.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { LeafletMap, LeafletTileLayer, LEAFLET_TOKEN } from 'src/app/leaflet/leaflet.service';
 import { MapPage } from './map.page';
+
 
 describe('MapPage', () => {
   let component: MapPage;
   let fixture: ComponentFixture<MapPage>;
 
   beforeEach(async(() => {
+    const testMap: LeafletMap = {
+      setView: () => null
+    };
+    const testTiles: LeafletTileLayer = {
+      options: {},
+      addTo: () => null
+    }
+    const mockLeaflet = {
+      map: () => testMap,
+      tileLayer: () => testTiles
+    }
     TestBed.configureTestingModule({
-      declarations: [ MapPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [MapPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: LEAFLET_TOKEN, useValue: mockLeaflet }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapPage);
