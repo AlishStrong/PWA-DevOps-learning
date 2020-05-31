ca370278fdc149f611e3edd7d726b44b512d8651
Alisher <alisher.aliev@tieto.com>
2020-05-31 21:13:34 +0300

    Initial implementation of map on Home-Map using Leaflet

--- src/app/pages/home/map/map.page.ts
+++ src/app/pages/home/map/map.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Leaflet, LeafletMap, LeafletTileLayer, LEAFLET_TOKEN } from 'src/app/leaflet/leaflet.service';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  map: LeafletMap;
+  tiles: LeafletTileLayer;
 
-  constructor() { }
+  constructor(@Inject(LEAFLET_TOKEN) private leaflet: Leaflet) { }
 
   ngOnInit() {
+    // Leaflet requires a timeout to render properly!
+    setTimeout(() => {
+      console.log('I 100 msec passed');
+      const mymap = this.leaflet.map('mapid');
+      console.log('create Map');
+      mymap.setView([60.1918347, 24.8373201], 13);
+      const tiles = this.leaflet.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          maxZoom: 19
+        }
+      );
+      tiles.addTo(mymap);
+    }, 10)
   }
 
 }
