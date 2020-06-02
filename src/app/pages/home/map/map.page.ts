e96094849e227fd7f26da23893e552bf11d28835
Alisher <alisher.aliev@tieto.com>
2020-06-02 17:37:33 +0300

    Add user's location icon only if location was obtained

--- src/app/pages/home/map/map.page.ts
+++ src/app/pages/home/map/map.page.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { LeafletMap, LeafletTileLayer, LEAFLET_TOKEN } from 'src/app/leaflet/leaflet.service';
 
+const helsinkiLatLong = [60.1699, 24.9384];
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -9,17 +10,45 @@ import { LeafletMap, LeafletTileLayer, LEAFLET_TOKEN } from 'src/app/leaflet/lea
 export class MapPage implements OnInit {
   map: LeafletMap;
   tiles: LeafletTileLayer;
-  userLocation: Position;
+  userLatLong: number[];
 
   constructor(@Inject(LEAFLET_TOKEN) private leaflet: any) { }
 
   ngOnInit() {
     this.obtainCurrentLocation();
+  }
+
+  private obtainCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.positionSuccess(),
+        this.positionError()
+      );
+    } else {
+      console.warn('User did not allow location obtainment');
+      this.renderLeafletMap();
+    }
+  }
+
+  private positionSuccess() {
+    return (position: Position) => {
+      console.log(position);
+      this.userLatLong = [position.coords.latitude, position.coords.longitude];
+      this.renderLeafletMap(this.userLatLong);
+    };
+  }
+
+  private positionError() {
+    return (positionError: PositionError) => {
+      console.error(positionError);
+      this.renderLeafletMap();
+    };
+  }
+
+  private renderLeafletMap(latlong?: number[]): void {
     // Leaflet requires a timeout to render properly!
     setTimeout(() => {
       const mymap = this.leaflet.map('mapid');
-
-      mymap.setView([60.1918347, 24.8373201], 13);
 
       this.leaflet.tileLayer(
         'https://cdn.digitransit.fi/map/v1/{id}/{z}/{x}/{y}@2x.png',
@@ -31,39 +60,23 @@ export class MapPage implements OnInit {
           id: 'hsl-map'
         }).addTo(mymap);
 
-      const customIcon = this.leaflet.icon({
-        iconUrl: '/svg/location-sharp.svg',
-        iconSize: [40, 40],
-        iconAnchor: [20, 39],
-        popupAnchor: [0.5, -20],
-      });
-
-      this.leaflet.marker([60.1918347, 24.8373201], { icon: customIcon }).bindPopup("<b>Du är här!</b><br>").openPopup().addTo(mymap);
+      // If user's location was obtained, location icon will be added  
+      if (latlong) {
+        mymap.setView(latlong, 13);
+        this.setUserLocationIcon(latlong).addTo(mymap);
+      } else {
+        mymap.setView(helsinkiLatLong, 13);
+      }
     }, 100)
   }
 
-  private obtainCurrentLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        this.positionSuccess(),
-        this.positionError()
-      );
-    } else {
-      console.warn('User did not allow location obtainment');
-    }
+  private setUserLocationIcon(latlong: number[]): any {
+    const customIcon = this.leaflet.icon({
+      iconUrl: '/svg/location-sharp.svg',
+      iconSize: [40, 40],
+      iconAnchor: [20, 39],
+      popupAnchor: [0.5, -20],
+    });
+    return this.leaflet.marker(latlong, { icon: customIcon }).bindPopup("<b>Du är här!</b><br>").openPopup();
   }
-
-  private positionSuccess() {
-    return (position: Position) => {
-      this.userLocation = position;
-      console.log(position);
-    };
-  }
-
-  private positionError() {
-    return (positionError: PositionError) => {
-      console.error(positionError);
-    };
-  }
-
 }
