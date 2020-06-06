076ae86c206dba2a6d9e22340b55783c793503d5
Alisher <alisher.aliev@tieto.com>
2020-06-06 14:41:48 +0300

    Place Markers (icons) react to filters from Home Page

--- src/app/pages/home/map/map.page.ts
+++ src/app/pages/home/map/map.page.ts
@@ -13,9 +13,10 @@ const helsinkiLatLong = [60.1699, 24.9384];
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit, OnDestroy {
-  map: any;
-  layer: any;
-  mapPlaces: Place[];
+  map: any; // should be Leaflet.Map
+  tileLayer: any; // should be Leaflet.Layer
+  places: Place[];
+  placeMarkerMap = new Map<string, any>(); // should be Map<string, Leaflet.Marker>
 
   placesSubscription: Subscription;
 
@@ -30,7 +31,7 @@ export class MapPage implements OnInit, OnDestroy {
       .pipe(
         tap((places: Place[]) => {
           // Step 1: get places
-          this.mapPlaces = places;
+          this.places = places;
         }),
         catchError(error => {
           console.error('Failed to get Places from Places Service', error);
@@ -40,42 +41,19 @@ export class MapPage implements OnInit, OnDestroy {
       )
       .subscribe(
         () => {
-          // Step 2: render the map
-          // Step 2.1: bind Map to html element of id 'mapid
-          this.map = this.leaflet.map('mapid');
+          // Step 2: render the map if it was not rendered yet!
+          if (!this.map) {
+            console.log('MapPage.onInit().subscription - rendering Map');
+            this.renderMap();
+          } else {
+            console.log('MapPage.onInit().subscription - Map already EXIST');
+          }
 
-          // Step 2.2: define Layer for the map
-          this.layer = this.leaflet.tileLayer(
-            'https://cdn.digitransit.fi/map/v1/{id}/{z}/{x}/{y}@2x.png',
-            {
-              attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-              maxZoom: 19,
-              tileSize: 512,
-              zoomOffset: -1,
-              id: 'hsl-map'
-            });
+          console.log('this.placeMarkerMap', this.placeMarkerMap.size);
 
-          // Step 2.3: add the Layer to the Map  
-          this.map.addLayer(this.layer);
-
-          // Step 2.4: locate user's position
-          this.map.locate({
-            setView: true
-          });
-
-          // Step 2.4.1: add location icon on locationfound-event
-          this.map.on('locationfound', (locationEvent) => {
-            this.map.setView(locationEvent.latlng, 13);
-            this.map.addLayer(this.setUserLocationIcon(locationEvent.latlng));
-          });
-          // Step 2.4.2: set view to Helsinki on locationerror-event
-          this.map.on('locationerror', (locationError) => {
-            this.map.setView(helsinkiLatLong, 13);
-          });
-
-          // Step 2.5: add icons of places
-          if (this.mapPlaces) {
-            this.setPlaceLocationIcon();
+          // Step 3: add/remove Markers of places
+          if (this.places) {
+            this.updatepPlaceMarkers();
           }
         }
       );
@@ -85,7 +63,108 @@ export class MapPage implements OnInit, OnDestroy {
     this.placesSubscription.unsubscribe();
   }
 
-  mapTypeToIcon(type: string): string {
+  private renderMap(): void {
+    // Step 2.1: bind Map to html element of id 'mapid
+    this.map = this.leaflet.map('mapid');
+
+    // Step 2.2: define Layer for the map
+    this.tileLayer = this.leaflet.tileLayer(
+      'https://cdn.digitransit.fi/map/v1/{id}/{z}/{x}/{y}@2x.png',
+      {
+        attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+        maxZoom: 19,
+        tileSize: 512,
+        zoomOffset: -1,
+        id: 'hsl-map'
+      });
+
+    // Step 2.3: add the Layer to the Map  
+    this.map.addLayer(this.tileLayer);
+
+    // Step 2.4: locate user's position
+    this.map.locate({
+      setView: true
+    });
+
+    // Step 2.4.1: add location icon on locationfound-event
+    this.map.on('locationfound', (locationEvent) => {
+      this.setUserLocationIcon(locationEvent.latlng);
+      this.map.setView(locationEvent.latlng, 13);
+    });
+    // Step 2.4.2: set view to Helsinki on locationerror-event
+    this.map.on('locationerror', (locationError) => {
+      console.log(locationError);
+      this.map.setView(helsinkiLatLong, 13);
+    });
+  }
+
+  private setUserLocationIcon(latlong: number[]) {
+    const customIcon = this.leaflet.divIcon({
+      iconSize: [40, 40],
+      className: 'customDivIcon',
+      iconAnchor: [20, 39],
+      popupAnchor: [0.5, -20],
+      html: '<ion-icon name="location-sharp" color="success"></ion-icon>'
+    });
+    this.leaflet.marker(latlong, { icon: customIcon }).bindPopup("<b>Du är här!</b><br>").openPopup().addTo(this.map);
+  }
+
+  private updatepPlaceMarkers() {
+    const newPlaces = new Set(this.places);
+    const currentTitles = new Set<string>(this.placeMarkerMap.keys());
+    const titlesRemove = new Set<string>(currentTitles);
+    const titlesAdd = new Set<string>();
+    const titlesKeep = new Set<string>();
+
+    for (let place of newPlaces) {
+      if (currentTitles.has(place.title)) {
+        titlesKeep.add(place.title);
+        titlesRemove.delete(place.title);
+      } else {
+        titlesAdd.add(place.title);
+        this.placeMarkerMap.set(place.title, this.setPlaceMarker(place));
+      }
+    }
+
+    for (let toRemove of titlesRemove) {
+      this.removePlaceMarker(toRemove);
+    }
+
+    console.log('titlesAdd', titlesAdd);
+    console.log('titlesRemove', titlesRemove);
+    console.log('titlesKeep', titlesKeep);
+    console.log('placeMarkerMap', this.placeMarkerMap);
+  }
+
+  private setPlaceMarker(place: Place): any {
+    const divIcon = this.leaflet.divIcon({
+      iconSize: [40, 40],
+      className: 'customDivIcon',
+      iconAnchor: [20, 39],
+      popupAnchor: [0.5, -20],
+      html: '<ion-icon name="location-sharp" color="danger"></ion-icon>'
+    });
+    return this.leaflet.marker(place.location, { icon: divIcon }).bindPopup(this.returnHTMLel(place)).openPopup().addTo(this.map);
+  }
+
+  private removePlaceMarker(placeTitle: string): void {
+    this.placeMarkerMap.get(placeTitle).remove();
+    this.placeMarkerMap.delete(placeTitle);
+  }
+
+  private returnHTMLel(place: Place): string {
+    var toReturn = `<div style="width: 100%"><b>${place.title}</b></div><div style="margin-top: 0; margin-bottom: 0; display: inline-block;">`;
+    place.type.forEach(t => {
+      const iconName = this.mapTypeToIcon(t);
+      toReturn += `
+      <div style="display: inline-block;"><ion-icon style="zoom:2.0" name="${iconName}"></ion-icon></div>
+      `;
+    })
+    toReturn += '</div>';
+    return toReturn;
+  }
+
+  private mapTypeToIcon(type: string): string {
     switch (type) {
       case 'active':
         return 'pulse';
@@ -110,44 +189,6 @@ export class MapPage implements OnInit, OnDestroy {
       default:
         return '';
     }
-  }
-
-  private setUserLocationIcon(latlong: number[]): any {
-    const customIcon = this.leaflet.divIcon({
-      iconSize: [40, 40],
-      className: 'customDivIcon',
-      iconAnchor: [20, 39],
-      popupAnchor: [0.5, -20],
-      html: '<ion-icon name="location-sharp" color="success"></ion-icon>'
-    });
-    return this.leaflet.marker(latlong, { icon: customIcon }).bindPopup("<b>Du är här!</b><br>").openPopup();
-  }
-
-  private setPlaceLocationIcon() {
-    this.mapPlaces.forEach(place => {
-      const divIcon = this.leaflet.divIcon({
-        iconSize: [40, 40],
-        className: 'customDivIcon',
-        iconAnchor: [20, 39],
-        popupAnchor: [0.5, -20],
-        html: '<ion-icon name="location-sharp" color="danger"></ion-icon>'
-      });
-
-      // bindPopup(content: String|HTMLElement|Function|Popup, options?: Popup options)
-      this.leaflet.marker(place.location, { icon: divIcon }).bindPopup(this.returnHTMLel(place)).openPopup().addTo(this.map);
-    });
-  }
-
-  private returnHTMLel(place: Place): string {
-    var toReturn = `<div style="width: 100%"><b>${place.title}</b></div><div style="margin-top: 0; margin-bottom: 0; display: inline-block;">`;
-    place.type.forEach(t => {
-      const iconName = this.mapTypeToIcon(t);
-      toReturn += `
-      <div style="display: inline-block;"><ion-icon style="zoom:2.0" name="${iconName}"></ion-icon></div>
-      `;
-    })
-    toReturn += '</div>';
-    return toReturn;
   }
 
   private prepareToast(message: string, color: string): Promise<HTMLIonToastElement> {
